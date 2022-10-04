43dc5e0a1a9af7acda07cf9daec5c05e4c800295
Andrew Regenscheid <andrew.regenscheid@nist.gov>
2022-10-03 23:08:37 -0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,16 @@
 
 if [ ! -z $INPUT_USERNAME ];
 then echo $INPUT_PASSWORD | docker login $INPUT_REGISTRY -u $INPUT_USERNAME --password-stdin
+echo 'input registry'
+echo $INPUT_REGISTRY
 fi
 
 if [ ! -z $INPUT_DOCKER_NETWORK ];
 then INPUT_OPTIONS="$INPUT_OPTIONS --network $INPUT_DOCKER_NETWORK"
+echo 'input options'
+echo $INPUT_OPTIONS
+echo 'input dockern network'
+echo $INPUT_DOCKER_NETWORK
 fi
 
 exec docker run $INPUT_OPTIONS --entrypoint=$INPUT_SHELL $INPUT_IMAGE -c "${INPUT_RUN//$'\n'/;}"
